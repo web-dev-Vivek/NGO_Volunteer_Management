99b73b40b2f7452185bff9138f13c3598040938c
Vivek Jha <progamervivek2020@gmail.com>
2026-08-02 21:16:30 +0530

    Fixes in app.js folder

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,16 +2,11 @@
 import express from "express"
 import cors from 'cors'
 import { errorHandler } from "./middleware/errorHandler.js"
-import authRoutes from './modules/auth/auth.routes.js';
-import taskRoutes from './modules/tasks/tasks.routes.js';
 
 const app = express();
 
 app.use(cors({ origin: process.env.CLIENT_URL || 'https://localhost:5173' }));
 app.use(express.json());
-
-app.use('/api/auth', authRoutes);
-app.use('/api/tasks', taskRoutes);
 
 app.use(errorHandler);
 
