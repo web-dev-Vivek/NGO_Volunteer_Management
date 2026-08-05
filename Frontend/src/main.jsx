abe81b8aac0a4de032bb52a2746a58c91b72eb7d
Vivek Jha <progamervivek2020@gmail.com>
2026-08-05 11:38:19 +0530

    .

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+<<<<<<< HEAD
 import './index.css'
+=======
+>>>>>>> eb47bd1 (Save current work)
 import { ClerkProvider } from '@clerk/react'
 import App from './App.jsx'
 
