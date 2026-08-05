5f299d9717e1db773808eb620212e8087eb4d86a
Vivek Jha <progamervivek2020@gmail.com>
2026-08-05 13:36:09 +0530

    import index in main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,9 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
 import './index.css'
-=======
->>>>>>> eb47bd1 (Save current work)
 import { ClerkProvider } from '@clerk/react'
 import App from './App.jsx'
 
